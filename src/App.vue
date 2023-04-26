cc057a465d7f70ea6112e7e2e27eeb0f50f5982b
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2023-04-26 23:18:27 +0700

    🔵 other: Decorate and organize the code.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref,onMounted, resolveDirective } from 'vue';
+import { ref, onMounted } from 'vue';
 import PhGlobe from './components/icons/PhGlobe.vue';
-import {getData} from './composable/getData.js';
+import { getData } from './composable/getData.js';
 
 
 const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const data =ref()
+const data = ref()
 
 onMounted(async () => {
   data.value = await getData();
@@ -17,40 +17,49 @@ onMounted(async () => {
 
 <template>
   <div class=" h-screen flex-auto" style="background-color: #F3F5F7; ">
-    <div style="width: 70em; " class="mx-auto">
-      <div class="text-center items-center justify-center">
-        <h1 class="text-3xl">SIT Announcement System(SAS)</h1>
-      </div>
-      <div class="flex mt-5">
-        <div class="flex bg-emerald-400 w-28 h-8 rounded-lg text-white ">
-          <PhGlobe /> Time zone:
+    <div style="width: 80em;" class="mx-auto">
+
+      <!-- header -->
+      <div class="text-center text-3xl text-cyan-800 py-10">SIT Announcement System(SAS)</div>
+
+      <!-- time zone bar -->
+      <div class="flex">
+        <div class="bg-emerald-500 flex p-1 pr-3 rounded-lg">
+          <PhGlobe class="pt-1" />
+          <div class="text-white text-lg">Time zone :</div>
         </div>
-        <div class="ml-2"> {{ timezone }}</div>
+        <div class="text-cyan-800 p-1 pl-3 text-lg">{{ timezone }}</div>
       </div>
 
       <hr class="mt-4 border-2">
 
-      <div class="grid grid-cols-7 mt-3 text-gray-400">
-        <div>No.</div>
-        <div>Title</div>
-        <div>Category</div>
-        <div>Publish Date</div>
-        <div>Close Date</div>
-        <div>Display</div>
-        <div>Action</div>
+      <!-- head table -->
+      <div class="grid grid-cols-10 my-7">
+        <div class="text-center text-zinc-400">No.</div>
+        <div class=" text-zinc-400 col-span-2">Title</div>
+        <div class="text-center text-zinc-400">Category</div>
+        <div class="text-center text-zinc-400 col-span-2">Publish Date</div>
+        <div class="text-center text-zinc-400 col-span-2">Close Date</div>
+        <div class="text-center text-zinc-400">Display</div>
+        <div class="text-center text-zinc-400">Action</div>
       </div>
 
-      <div class="text-center items-center justify-center text-gray-400 mt-48 text-2xl" v-if="data === undefined">No Announcement</div>
+      <div class="text-center items-center justify-center text-gray-400 mt-48 text-2xl" v-if="data === undefined">No
+        Announcement</div>
       <div v-else>
-        <div v-for="announce in data">
-          <div class="grid grid-cols-7 bg-white mt-3"> 
-            <div>{{ announce.announcementId }} </div>
-            <div>{{ announce.announcementTitle }} </div>
-            <div>{{ announce.categoriesObject.categoryName }}</div>
-            <div>{{ announce.publishDate }} </div> 
-            <div>{{ announce.closeDate }} </div>
-            <diV>{{ announce.announcementDisplay}}</diV>
-
+        <div v-for="(announce, index) in data" :key="data.announcementId"
+          class="grid grid-cols-10 bg-white my-5 h-20 rounded-xl shadow-md">
+          <div class="text-cyan-800 my-auto text-center">{{ announce.announcementId }} </div>
+          <div class="text-cyan-800 my-auto col-span-2">{{ announce.announcementTitle }} </div>
+          <div class="text-cyan-800 my-auto text-center capitalize">{{ announce.categoriesObject.categoryName }}</div>
+          <div class="text-cyan-800 my-auto text-center col-span-2">{{ announce.publishDate }} </div>
+          <div class="text-cyan-800 my-auto text-center col-span-2">{{ announce.closeDate }} </div>
+          <diV class="text-cyan-800 pt-2 my-auto mx-auto text-center w-10 h-10 rounded-full"
+            :class="announce.announcementDisplay == 'Y' ? 'bg-emerald-100 text-emerald-400' : 'bg-red-100 text-red-400'">
+            {{
+              announce.announcementDisplay }}
+          </diV>
+          <div class="text-cyan-400 my-auto text-center mx-auto bg-cyan-100 rounded-lg pt-2 w-20 h-10 shadow-md">View
           </div>
         </div>
       </div>
