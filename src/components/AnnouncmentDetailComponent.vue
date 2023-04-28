287cf477a9e80ba7a8baf124cb948c4131ad0d95
tana <tanasit.istg@mail.kmutt.ac.th>
2023-04-29 01:21:39 +0700

    🐛 fix(AnnouncmentDetailComponent.vue): replace detail with data to display announcement data correctly ✨ feat(AnnouncmentDetailComponent.vue): add back button to navigate back to announcements page

--- src/components/AnnouncmentDetailComponent.vue
+++ src/components/AnnouncmentDetailComponent.vue
@@ -1,43 +1,62 @@
 <script setup>
-const props = defineProps({
-    detail: {
-        type: Object
-    }
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { getDataById } from '../composable/getData';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const { params } = useRoute()
+const data = ref([])
+
+const backToAnnouncements = () => {
+    router.push({ name: 'Announcements' })
+}
+
+onMounted(async () => {
+    data.value = await getDataById(parseInt(params.id))
 })
 </script>
  
 <template>
-    <div>
+    <div style="width: 80em;" class="mx-auto">
+        <!-- header -->
         <header class="my-3">
             <h1 class="text-cyan-800 text-3xl">Announcement Detail</h1>
         </header>
+
+        <!-- content -->
         <div class="bg-white flex-col rounded-lg p-10 shadow-lg">
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Title</div>
-                <div class="text-cyan-800 w-full">{{ detail?.announcementTitle }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.announcementTitle }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Category</div>
-                <div class="text-cyan-800 w-full">{{ detail?.categoriesObject?.categoryName }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.categoriesObject?.categoryName }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Description</div>
-                <div class="text-cyan-800 w-full">{{ detail?.announcementDescription }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.announcementDescription }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Publish Date</div>
-                <div class="text-cyan-800 w-full">{{ detail?.publishDate || '-' }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.publishDate || '-' }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Close Date</div>
-                <div class="text-cyan-800 w-full">{{ detail?.closeDate || '-' }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.closeDate || '-' }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Display</div>
-                <div class="text-cyan-800 w-full">{{ detail?.announcementDisplay }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.announcementDisplay }}</div>
             </div>
         </div>
 
+        <!-- button -->
+        <div class="flex justify-end mt-3">
+            <button class="text-cyan-400 bg-cyan-100 text-center rounded-lg shadow-md cursor-pointer px-5 py-2 w-20 h-10"
+                @click="backToAnnouncements">Back</button>
+        </div>
     </div>
 </template>
  
