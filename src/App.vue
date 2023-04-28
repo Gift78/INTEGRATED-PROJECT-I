01ea8e231302189ebc36364d959b0e631063c53c
tana <tanasit.istg@mail.kmutt.ac.th>
2023-04-29 01:22:03 +0700

    💄 style(App.vue): remove unnecessary flex-auto class from main div and adjust indentation to improve readability

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template >
-  <div class=" h-screen flex-auto overflow-auto" style="background-color: #F3F5F7; ">
+  <div class="h-screen overflow-auto" style="background-color: #F3F5F7;">
     <RouterView />
   </div>
 </template>
