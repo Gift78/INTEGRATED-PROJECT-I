964cb94b13de5f747f862ffc925d8b0d3e7a04e6
tana <tanasit.istg@mail.kmutt.ac.th>
2023-04-29 01:20:55 +0700

    ✨ feat(getData.js): rename getData function to getAllData to improve semantics and add new function getDataById to fetch data by id from API

--- src/composable/getData.js
+++ src/composable/getData.js
@@ -1,4 +1,4 @@
-const getData = async () => {
+const getAllData = async () => {
   try {
     const res = await fetch(
       import.meta.env.VITE_ROOT_API + "/api/announcements"
@@ -20,4 +20,26 @@ const getData = async () => {
   }
 };
 
-export { getData };
+const getDataById = async (id) => {
+  try {
+    const res = await fetch(
+      import.meta.env.VITE_ROOT_API + "/api/announcements/" + id
+    );
+    if (res.ok) {
+      const data = await res.json();
+      return data;
+    } else if (res.status === 404) {
+      throw new Error("404 Not Found");
+    } else if (res.status === 500) {
+      throw new Error("500 Internal Server Error");
+    } else if (res.status === 503) {
+      throw new Error("503 Service Unavailable");
+    } else {
+      throw new Error("Something went wrong.");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { getAllData, getDataById };
