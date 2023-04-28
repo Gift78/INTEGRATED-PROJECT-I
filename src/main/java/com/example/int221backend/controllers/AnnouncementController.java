b8fa0813fd21f0cfee21fe6bbfdd0e083a257de8
tana <tanasit.istg@mail.kmutt.ac.th>
2023-04-28 16:25:19 +0700

    test

--- src/main/java/com/example/int221backend/controllers/AnnouncementController.java
+++ src/main/java/com/example/int221backend/controllers/AnnouncementController.java
@@ -4,10 +4,7 @@ import com.example.int221backend.entities.Announces;
 import com.example.int221backend.services.AnnounceService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
