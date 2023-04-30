2d23fb80834fdf04f9362836359cbe208609a993
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-01 02:17:17 +0700

    🐛 fix(GlobalException.java): handle AnnounceNotFoundException and MethodArgumentTypeMismatchException with custom error response

--- src/main/java/com/example/int221backend/exceptions/GlobalException.java
+++ src/main/java/com/example/int221backend/exceptions/GlobalException.java
@@ -1,2 +1,26 @@
-package com.example.int221backend.exceptions;public class GlobalException {
+package com.example.int221backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalException extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(AnnounceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleAnnounceNotFoundException(AnnounceNotFoundException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false).substring(4), HttpStatus.NOT_FOUND.getReasonPhrase());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getDescription(false).substring(4), HttpStatus.BAD_REQUEST.getReasonPhrase());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 }
