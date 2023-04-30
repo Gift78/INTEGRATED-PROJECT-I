ec83248aad7154171545a97f5d7cb69e793b5fb7
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-01 02:57:23 +0700

    🐛 fix(TimezoneComponent.vue): correct typo in Timezone word to improve semantics and readability

--- src/components/TimezoneComponent.vue
+++ src/components/TimezoneComponent.vue
@@ -8,7 +8,7 @@ const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     <div class="flex">
         <div class="flex p-1 pr-3 rounded-lg" style="background-color: #12C980;">
             <IconGlobe class="pt-1" />
-            <div class="text-white text-lg">Time zone :</div>
+            <div class="text-white text-lg">Timezone :</div>
         </div>
         <div class="text-cyan-800 p-1 pl-3 text-lg">{{ timezone }}</div>
     </div>
