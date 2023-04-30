47cbec01c634b1b9ff9236a77564d5d3b942910d
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-01 02:18:00 +0700

    🐛 fix(AnnounceNotFoundException.java): add HttpStatus.NOT_FOUND as response status and improve exception message to include the id of the announcement that was not found

--- src/main/java/com/example/int221backend/exceptions/AnnounceNotFoundException.java
+++ src/main/java/com/example/int221backend/exceptions/AnnounceNotFoundException.java
@@ -1,2 +1,11 @@
-package com.example.int221backend.exceptions;public class AnnounceNotFoundException {
+package com.example.int221backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class AnnounceNotFoundException extends RuntimeException {
+    public AnnounceNotFoundException(Integer id) {
+        super("Announcement id " + id + " does not exist!.");
+    }
 }
