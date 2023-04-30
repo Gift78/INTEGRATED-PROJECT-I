dd08fd81a167d4ed887a9ae7e6e083fc25978a57
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-01 00:18:17 +0700

    🐛 fix(AnnouncmentDetailComponent.vue): handle undefined data value and show error modal when data is undefined

--- src/components/AnnouncmentDetailComponent.vue
+++ src/components/AnnouncmentDetailComponent.vue
@@ -1,22 +1,31 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import { useRouter } from 'vue-router'
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
 import { getDataById } from '../composable/getData';
 import formatDatetime from '../composable/formatDatetime';
 import TimezoneComponent from './TimezoneComponent.vue';
 
-const { params } = useRoute()
-const router = useRouter()
-const data = ref({})
+const { params } = useRoute();
+const router = useRouter();
+const data = ref({});
+const isModalOpen = ref(false);
 
 const backToAnnouncements = () => {
-    router.push({ name: 'Announcement' })
+    router.push({ name: 'Announcement' });
 }
 
 onMounted(async () => {
-    data.value = await getDataById(parseInt(params.id))
-})
+    try {
+        data.value = await getDataById(params?.id);
+
+        if (data.value === undefined) {
+            isModalOpen.value = true;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
 </script>
  
 <template>
@@ -27,37 +36,49 @@ onMounted(async () => {
         <TimezoneComponent />
 
         <!-- content -->
-        <div class="bg-white flex-col rounded-lg p-10 shadow-lg mt-5">
+        <div class="bg-white flex-col rounded-lg p-10 shadow-lg mt-5" v-if="!isModalOpen">
             <div class="flex">
                 <div class="w-52 text-cyan-800 font-bold">Title</div>
-                <div class="text-cyan-800 w-full">{{ data.announcementTitle }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.announcementTitle }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Category</div>
-                <div class="text-cyan-800 w-full">{{ data.announcementCategory }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.announcementCategory }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Description</div>
-                <div class="text-cyan-800 w-full">{{ data.announcementDescription }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.announcementDescription }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Publish Date</div>
-                <div class="text-cyan-800 w-full">{{ formatDatetime(data.publishDate) || '-' }}</div>
+                <div class="text-cyan-800 w-full">{{ formatDatetime(data?.publishDate) || '-' }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Close Date</div>
-                <div class="text-cyan-800 w-full">{{ formatDatetime(data.closeDate) || '-' }}</div>
+                <div class="text-cyan-800 w-full">{{ formatDatetime(data?.closeDate) || '-' }}</div>
             </div>
             <div class="flex mt-5">
                 <div class="w-52 text-cyan-800 font-bold">Display</div>
-                <div class="text-cyan-800 w-full">{{ data.announcementDisplay }}</div>
+                <div class="text-cyan-800 w-full">{{ data?.announcementDisplay }}</div>
             </div>
         </div>
 
         <!-- button -->
-        <div class="flex justify-end mt-3">
+        <div class="flex justify-end mt-3" v-if="!isModalOpen">
             <button class="text-cyan-400 bg-cyan-100 text-center rounded-lg shadow-md cursor-pointer px-5 py-2 w-20 h-10"
                 @click="backToAnnouncements">Back</button>
+        </div>
+    </div>
+
+    <input type="checkbox" id="error-modal" class="modal-toggle modal-open" :checked="isModalOpen" />
+    <div class="modal">
+        <div class="modal-box bg-white">
+            <h3 class="font-bold text-2xl text-black">Error!</h3>
+            <p class="pt-4 text-black">The requested page is not available!</p>
+            <div class="modal-action">
+                <label for="error-modal" class="btn text-black bg-white hover:bg-gray-200"
+                    @click="backToAnnouncements">OK</label>
+            </div>
         </div>
     </div>
 </template>
