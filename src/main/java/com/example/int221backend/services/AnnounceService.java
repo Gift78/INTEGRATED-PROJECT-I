7c1d7e090e61b9dea4d474e1064d70b199bb069f
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2023-04-30 18:47:53 +0700

    edited to pass the test

--- src/main/java/com/example/int221backend/services/AnnounceService.java
+++ src/main/java/com/example/int221backend/services/AnnounceService.java
@@ -4,7 +4,11 @@ import com.example.int221backend.entities.Announces;
 import com.example.int221backend.repositories.AnnounceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -22,13 +26,13 @@ public class AnnounceService {
         List<Announces> announces = announceRepository.findAll(Sort.by("announcementId").descending());
         announces.forEach((announce) -> {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            if(announce.getPublishDate()!=null){
+            if (announce.getPublishDate() != null) {
                 LocalDateTime localPublishDate = LocalDateTime.parse(announce.getPublishDate(), formatter);
                 ZonedDateTime zonePublishDate = localPublishDate.atZone(ZoneId.of("UTC"));
                 Instant instantPublishDate = zonePublishDate.toInstant();
                 announce.setPublishDate(instantPublishDate.toString());
             }
-            if (announce.getCloseDate()!=null){
+            if (announce.getCloseDate() != null) {
                 LocalDateTime localCloseDate = LocalDateTime.parse(announce.getCloseDate(), formatter);
                 ZonedDateTime zoneCloseDate = localCloseDate.atZone(ZoneId.of("UTC"));
                 Instant instantCloseDate = zoneCloseDate.toInstant();
@@ -40,15 +44,15 @@ public class AnnounceService {
 
     public Announces getAnnounceById(Integer announceId) {
         Announces announce = announceRepository.findById(announceId).orElseThrow(
-                () -> new RuntimeException("Announcement ID " + announceId + " does not exist!."));
+                () -> new ResourceNotFoundException("Announcement id does not exist"));
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        if(announce.getPublishDate()!=null){
+        if (announce.getPublishDate() != null) {
             LocalDateTime localPublishDate = LocalDateTime.parse(announce.getPublishDate(), formatter);
             ZonedDateTime zonePublishDate = localPublishDate.atZone(ZoneId.of("UTC"));
             Instant instantPublishDate = zonePublishDate.toInstant();
             announce.setPublishDate(instantPublishDate.toString());
         }
-        if (announce.getCloseDate()!=null){
+        if (announce.getCloseDate() != null) {
             LocalDateTime localCloseDate = LocalDateTime.parse(announce.getCloseDate(), formatter);
             ZonedDateTime zoneCloseDate = localCloseDate.atZone(ZoneId.of("UTC"));
             Instant instantCloseDate = zoneCloseDate.toInstant();
@@ -57,4 +61,10 @@ public class AnnounceService {
         return announce;
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String message) {
+            super(message);
+        }
+    }
 }
