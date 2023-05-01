754608feec99f3d2d3004ab70920c752da882795
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-01 21:27:49 +0700

    🐛 fix(AnnouncmentsListingComponent.vue): update the text displayed when there are no announcements to match the text used in the test case.

--- src/components/AnnouncmentsListingComponent.vue
+++ src/components/AnnouncmentsListingComponent.vue
@@ -40,7 +40,7 @@ onMounted(async () => {
 
             <div class="text-center items-center justify-center text-gray-400 mt-48 text-2xl"
                 v-if="data === undefined || data.length === 0">
-                No Announcements
+                No Announcement
             </div>
             <div v-else>
                 <!-- show data -->
