203efa0e2f702a49fe7216b30beff42c07309638
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-03 00:06:46 +0700

    🐛 fix(router/index.js): add import.meta.env.BASE_URL to createWebHistory to correctly handle subdirectory routing

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import AnnouncmentsListingComponent from "../components/AnnouncmentsListingCompo
 import AnnouncmentsDetailComponent from "../components/AnnouncmentDetailComponent.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
