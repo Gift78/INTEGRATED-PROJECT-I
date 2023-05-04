57509f7f5a2fe99e4040e74e5af65565a78f403c
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-05 02:44:25 +0700

    🚀 chore(cypress.config.cjs): update baseUrl to point to the correct URL for the application's new location on the server

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
-    baseUrl: "http://ip22kw2.sit.kmutt.ac.th/",
+    baseUrl: "http://intproj22.sit.kmutt.ac.th/kw2/",
     experimentalSessionAndOrigin: true,
   },
 });
