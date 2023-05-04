bc408138ec3b168da0c5ff34b791cec7e81777d8
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-04 18:47:35 +0700

    🎨 style(AnnouncmentsListingComponent.vue): add cursor pointer to the add announcement button to improve user experience

--- src/components/AnnouncmentsListingComponent.vue
+++ src/components/AnnouncmentsListingComponent.vue
@@ -32,7 +32,7 @@ onMounted(async () => {
             <!-- time zone bar -->
             <div class="flex justify-between">
                 <TimezoneComponent />
-                <div class="ann-button text-center rounded-md text-white p-1 px-4 bg-emerald-plus hover:bg-emerald-light hover:scale-105"
+                <div class="ann-button text-center rounded-md text-white p-1 px-4 bg-emerald-plus hover:bg-emerald-light hover:scale-105 cursor-pointer"
                     @click="changePage('AddAnnouncement', undefined)">
                     + Add Announcement
                 </div>
