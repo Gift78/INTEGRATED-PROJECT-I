fdb913d9134443e8f6ab9a0a286213b458998960
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-05 19:47:17 +0700

    ✨ feat(AnnounceTestDTO.java): add AnnounceTestDTO class to represent announcement data transfer object

---
+++ src/main/java/com/example/int221backend/dtos/AnnounceTestDTO.java
@@ -0,0 +1,22 @@
+package com.example.int221backend.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnnounceTestDTO {
+    private Integer id;
+    private String announcementTitle;
+    private String announcementDescription;
+    private String publishDate;
+    private String closeDate;
+    private String announcementDisplay;
+    @JsonIgnore
+    private CategoryDTO categoriesObject;
+    public String getAnnouncementCategory(){
+        return categoriesObject.getCategoryName();
+    }
+    private Integer categoryId;
+}
