8aed6d119e76745d38e112a7a66cf0aabf197b4a
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-05 19:46:02 +0700

    ✨ feat(AnnouncementController.java): add AnnounceTestDTO to create and update methods to return announcement with category name and id

--- src/main/java/com/example/int221backend/controllers/AnnouncementController.java
+++ src/main/java/com/example/int221backend/controllers/AnnouncementController.java
@@ -2,10 +2,15 @@ package com.example.int221backend.controllers;
 
 import com.example.int221backend.dtos.AnnounceDTO;
 import com.example.int221backend.dtos.AnnounceDetailDTO;
+import com.example.int221backend.dtos.AnnounceTestDTO;
 import com.example.int221backend.entities.Announces;
+import com.example.int221backend.entities.Categories;
+import com.example.int221backend.repositories.CategoryRepository;
 import com.example.int221backend.services.AnnounceService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,22 +23,31 @@ public class AnnouncementController {
     private AnnounceService announceService;
     @Autowired
     private ModelMapper modelMapper;
+
     @GetMapping("")
     public List<AnnounceDTO> getAllAnnouncements() {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         List<Announces> announces = announceService.getAllAnnouncements();
-        List<AnnounceDTO> announceDTOList = announces.stream().map(e -> modelMapper.map(e, AnnounceDTO.class)).collect(Collectors.toList());
+        List<AnnounceDTO> announceDTOList = announces.stream().map(e -> {
+            AnnounceDTO announceDTO = modelMapper.map(e, AnnounceDTO.class);
+            announceDTO.setId(e.getAnnouncementId());
+            return announceDTO;
+        }).collect(Collectors.toList());
         return announceDTOList;
     }
 
     @GetMapping("{announceId}")
     public AnnounceDetailDTO getDetailById(@PathVariable Integer announceId) {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         Announces announcesExist = announceService.getAnnounceById(announceId);
         return modelMapper.map(announcesExist,AnnounceDetailDTO.class);
     }
 
     @PostMapping("")
-    public Announces create(@RequestBody Announces newAnnounce) {
-        return announceService.addNewAnnounce(newAnnounce);
+    public AnnounceTestDTO create(@RequestBody Announces newAnnounce) {
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        Announces announce = announceService.addNewAnnounce(newAnnounce);
+        return modelMapper.map(announce, AnnounceTestDTO.class);
     }
 
     @DeleteMapping("{announceId}")
@@ -42,7 +56,9 @@ public class AnnouncementController {
     }
 
     @PutMapping("{announceId}")
-    public Announces updateAnnounce(@PathVariable Integer announceId, @RequestBody Announces newAnnounce){
-        return announceService.updateAnnounce(announceId, newAnnounce);
+    public AnnounceTestDTO updateAnnounce(@PathVariable Integer announceId, @RequestBody Announces newAnnounce){
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        Announces announce = announceService.updateAnnounce(announceId, newAnnounce);
+        return modelMapper.map(announce, AnnounceTestDTO.class);
     }
 }
