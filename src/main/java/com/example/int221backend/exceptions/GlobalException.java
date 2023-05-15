df873edce77e5d75c02e92dc213673a8bf24e963
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-15 12:30:58 +0700

    feat: category now throw ConstraintViolationException

--- src/main/java/com/example/int221backend/exceptions/GlobalException.java
+++ src/main/java/com/example/int221backend/exceptions/GlobalException.java
@@ -20,30 +20,16 @@ public class GlobalException extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
-//    @ExceptionHandler(CategoryNotFoundException.class)
-//    public ResponseEntity<ErrorResponse> handleCategoryNotFoundException(CategoryNotFoundException ex, WebRequest request) {
-//        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false).substring(4), HttpStatus.NOT_FOUND.getReasonPhrase());
-//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-//    }
-
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
         Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Validation Error", request.getDescription(false).substring(4), HttpStatus.BAD_REQUEST.getReasonPhrase());
-        for (ConstraintViolation<?> constraintViolation : constraintViolations) {
-            errorResponse.addValidationError(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+        if (constraintViolations != null) {
+            for (ConstraintViolation<?> constraintViolation : constraintViolations) {
+                errorResponse.addValidationError(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+            }
         }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleAllException(Exception ex, WebRequest request) {
-        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage(), request.getDescription(false).substring(4), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-        Set<ConstraintViolation<?>> constraintViolations = ((ConstraintViolationException) ex).getConstraintViolations();
-        for (ConstraintViolation<?> constraintViolation : constraintViolations) {
-            errorResponse.addValidationError(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
-        }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
