0463140c8aa5ed31a038e5d41ed738222d7d8fdd
tana <tanasit.istg@mail.kmutt.ac.th>
2023-05-19 10:30:27 +0700

    chore: use ControllerAdvice

--- src/main/java/com/example/int221backend/exceptions/GlobalException.java
+++ src/main/java/com/example/int221backend/exceptions/GlobalException.java
@@ -4,15 +4,15 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Set;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalException extends ResponseEntityExceptionHandler {
     @ExceptionHandler(AnnounceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleAnnounceNotFoundException(AnnounceNotFoundException ex, WebRequest request) {
