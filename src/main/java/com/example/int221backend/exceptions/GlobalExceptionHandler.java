f761b499e2d5c3a596447575edc2645731f1c9b4
tana <tanasit.istg@mail.kmutt.ac.th>
2023-06-01 07:49:25 +0700

    chore: renamed to GlobalExceptionHandler

--- src/main/java/com/example/int221backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/int221backend/exceptions/GlobalExceptionHandler.java
@@ -4,23 +4,21 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Set;
 
-@ControllerAdvice
-public class GlobalException extends ResponseEntityExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(AnnounceNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleAnnounceNotFoundException(AnnounceNotFoundException ex, WebRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getDescription(false).substring(4), HttpStatus.NOT_FOUND.getReasonPhrase());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
     @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
         Set<ConstraintViolation<?>> constraintViolations = ex.getConstraintViolations();
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Announcement attributes validation failed!", request.getDescription(false).substring(4), HttpStatus.BAD_REQUEST.getReasonPhrase());
